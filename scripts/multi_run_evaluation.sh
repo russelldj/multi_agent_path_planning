76eb8e4b7959d2e22d1ce2f10169c52797cab501
David Russell <davidrussell327@gmail.com>
2023-04-17 14:46:14 -0400

    Remove unneeded flag in runner script

--- scripts/multi_run_evaluation.sh
+++ scripts/multi_run_evaluation.sh
@@ -1,2 +1,2 @@
-python multi_agent_path_planning/lifelong_MAPF/multirun_evaluation.py --timeout 60
+python multi_agent_path_planning/lifelong_MAPF/multirun_evaluation.py 
 python multi_agent_path_planning/lifelong_MAPF/multirun_evaluation.py --vis-existing
